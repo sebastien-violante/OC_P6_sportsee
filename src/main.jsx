e65c012c75385ff0bbe34c98fe70a9e8987c4ef5
sebastien-violante <sebastien.violante@gmail.com>
2026-04-21 11:39:42 +0200

    mise ne place React Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home.jsx';
+import Error from './pages/Error.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+    </Router>
   </StrictMode>,
 )
