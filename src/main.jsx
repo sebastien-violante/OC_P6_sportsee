6473c870a3aaa9bcd4088bea1cab60021c4704e6
sebastien-violante <sebastien.violante@gmail.com>
2026-04-25 21:54:41 +0200

    mise en place authentification et redirection

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
 import Error from './pages/Error.jsx';
-import "@fontsource/inter"; // poids normal (400)
+import Layout from './pages/Layout.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Profil from './pages/Profil.jsx';
+import "@fontsource/inter"; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route path="/dashboard" index element={<Dashboard />} />
+            <Route path="/profil" element={<Profil />} />
+          </Route>
           <Route path="*" element={<Error />} />
         </Routes>
     </Router>
---
+++ src/pages/Profil.jsx
@@ -0,0 +1,7 @@
+export default function Profil() {
+    return (
+        <>
+            Profil
+        </>
+    )
+}
