e80bbfecdf20825f95bc5c33cf6cd99433934872
sebastien-violante <sebastien.violante@gmail.com>
2026-05-04 21:27:47 +0200

    suppression code inutile

--- src/api/services/formatUser.js
+++ src/api/services/formatUser.js
@@ -2,20 +2,9 @@ import { DateTime } from "luxon"
 
 export function formatUser(user, useMock) {
 
-  if (!user || !user.profile) {
-    return {
-      userId: "",
-      memberDate: null,
-      totalDistance: 0,
-      userPicture: "defaultUser.jpeg"
-    }
-  }
-
   const userId = `${user.profile.firstName} ${user.profile.lastName}`
   const memberDate = DateTime.fromISO(user.profile.createdAt)
-
   const userPicture = useMock ? user.profile.profilePicture.split('/').pop() : user.profile.profilePicture
-  console.log(user)
   const totalDistance = Math.floor(Number(user.statistics.totalDistance))
 
   return { userId, memberDate, totalDistance, userPicture }
