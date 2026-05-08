c4eb1729a1a5ef82f60452dcf1364efb7c2aa2d9
sebastien-violante <sebastien.violante@gmail.com>
2026-05-08 16:19:45 +0200

    résolution bug fix useMock en entrée d'application

--- src/components/ProfileBadge/ProfileBadge.jsx
+++ src/components/ProfileBadge/ProfileBadge.jsx
@@ -3,7 +3,9 @@ import './ProfileBadge.css'
 export default function ProfileBadge({picture, id, date}) {
     return (
          <article className="identity">
-          <img className="pictureId" src={picture} alt="image de profil"></img>
+          <div className="pictureWrapper">
+            <img className="pictureId" src={picture} alt="image de profil"></img>
+          </div>
           <div className="dataId">
             <h1>{id}</h1>
             <p className="caption">{date ? `Membre depuis le ${date.setLocale('fr').toFormat('d LLLL yyyy')}` : "" }</p>
